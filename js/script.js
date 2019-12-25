5bf26b686ff60940c6ea2ff195e057acee5ce081
Negorizont <boltuev01@mail.ru>
2019-12-25 11:32:58 +0300

    add Modal func

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 
   //Timer
-  let deadline = '2018-10-21';                                                                                      
+  let deadline = '2020-01-01';                                                                                      
 
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -80,5 +80,23 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   }
   setClock('timer', deadline);
+
+
+  // Modal
+  let more = document.querySelector('.more'),
+      overlay = document.querySelector('.overlay'),
+      close = document.querySelector('.popup-close');
+
+  more.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  });
+
+  close.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = 'scroll';
+  });
 
 });
