afea3b25c0d23b96468484b29c1c9df4609891e2
Negorizont <boltuev01@mail.ru>
2019-12-26 16:09:41 +0300

    add Form request

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
   function showTabContent(b) {
     if (tabContent[b].classList.contains('hide')) {
-      tabContent[i].classList.remove('hide');
-      tabContent[i].classList.add('show');
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
     }
   }
 
@@ -99,4 +99,50 @@ window.addEventListener('DOMContentLoaded', function() {
     document.body.style.overflow = 'scroll';
   });
 
+  // Form
+  let message = {
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+  };
+
+  let form = document.querySelector('.main-form'),
+      input = form.getElementsByTagName('input'),
+      statusMessage = document.createElement('div');
+
+      statusMessage.classList.add('status');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+
+    let formData = new FormData(form);
+    let obj = {};
+    formData.forEach(function(value, key) {
+      obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+    request.send(json);
+
+
+    request.addEventListener('readystatechange', function() {
+      if (request.readyState < 4) {
+        statusMessage.innerText = message.loading;
+      } else if (request.readyState === 4 && request.status === 200) {
+        statusMessage.innerText = message.success;
+      } else {
+        statusMessage.innerText = message.failure;
+      }
+    });
+
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = '';
+    }
+
+  });
 });
